79e313b9224f5a96874861ba4d0d62c277626e41
sanich123 <aovoronin.piano@gmail.com>
2022-10-02 20:19:28 +0300

    feat: some work on mobile version

--- source/js/nav-active-toggler.js
+++ source/js/nav-active-toggler.js
@@ -1,3 +1,17 @@
 const navLinks = document.querySelectorAll('.nav-list__item a')
-console.log(navLinks);
-console.log(window.location.href)
+const footerLinks = document.querySelectorAll('.footer-nav-list__item a');
+const index = 'index.html';
+const donate = 'donate.html';
+const currentLocation = window.location.href.slice(22);
+
+for (let i = 0; i < navLinks.length; i++) {
+  const current = navLinks[i];
+  if (currentLocation === donate) {
+      if (current.innerHTML === 'Donate') {
+        current.classList.add('active');
+      } else {
+        current.classList.remove('active');
+      }
+    }
+  }
+
