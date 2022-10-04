5d628097c505cfd7a44da984b0c69a8294ca5f62
sanich123 <aovoronin.piano@gmail.com>
2022-10-04 15:33:41 +0300

    feat: some work with header

--- source/js/menu-toggler.js
+++ source/js/menu-toggler.js
@@ -1,6 +1,23 @@
-const burgerBtn = document.querySelector('.header__menu');
-const header = document.querySelector('.header');
-burgerBtn.addEventListener('click', () => {
-  header.classList.add('open--menu');
-  header.classList.remove('header');
+const burgerBtn = document.querySelector(".header__menu");
+const closeBtn = document.querySelector(".header__close-btn");
+const headerWrapper = document.querySelector(".another__wrapper");
+const header = document.querySelector(".header");
+const modal = document.querySelector(".modal");
+
+burgerBtn.addEventListener("click", () => {
+  header.classList.add("open--menu");
+  header.classList.remove("header");
+  modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  header.classList.add("header");
+  modal.style.display = "none";
+  header.classList.remove("open--menu");
+});
+
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
+  header.classList.add("header");
+  header.classList.remove("open--menu");
 });
