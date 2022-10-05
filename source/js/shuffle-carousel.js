0a89c461101ba11ff86ebb4e30eced23cc967f08
Alexander Voronin <aovoronin.piano@gmail.com>
2022-10-05 22:33:19 +0300

    feat: almost done with carousel

--- source/js/shuffle-carousel.js
+++ source/js/shuffle-carousel.js
@@ -65,13 +65,18 @@ const rightBtn = document.querySelector('.cards__btn-right');
 const leftBtn = document.querySelector('.cards__btn-left');
 
 rightBtn.addEventListener('click', () => {
+  cards.classList.add('slide-right');
   const shuffled = data.slice().sort(() => 0.5 - Math.random());
   shufflePets(shuffled);
+  setTimeout(() => cards.classList.remove('slide-right'), 500);
+
 });
 
 leftBtn.addEventListener('click', () => {
+  cards.classList.add('slide-left');
   const shuffled = data.slice().sort(() => 0.5 - Math.random());
   shufflePets(shuffled);
+  setTimeout(() => cards.classList.remove('slide-left'), 500);
 })
 
 
