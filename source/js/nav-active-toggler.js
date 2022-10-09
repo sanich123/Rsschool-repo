86043f3087807fdec79ce9c869d5f9724564035d
Alexander Voronin <aovoronin.piano@gmail.com>
2022-10-09 22:07:07 +0300

    feat: almost done with js

--- source/js/nav-active-toggler.js
+++ source/js/nav-active-toggler.js
@@ -1,8 +1,9 @@
+import { currentLocation, donatePage } from "./const.js";
 const navLinks = document.querySelectorAll('.nav-list__item a')
 
 for (let i = 0; i < navLinks.length; i++) {
   const current = navLinks[i];
-  if (currentLocation === donatePage) {
+  if (currentLocation.includes(donatePage)) {
       if (current.innerHTML === 'Donate') {
         current.classList.add('active');
       } else {
