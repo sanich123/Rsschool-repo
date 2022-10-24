507464678ee7391fff0cc6d37570af76eb2ce7fe
sanich123 <aovoronin.piano@gmail.com>
2022-10-24 04:59:32 +0300

    feat: experiments

--- src/js/entry.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable linebreak-style */
-
-import '@babel/polyfill';
-import '../index.html';
-import '../less/entry.less';
-import { defaultValue } from './utils/const';
-import CreateGame from './createGame/createGame';
-
-CreateGame(defaultValue, 4, document.documentElement.clientWidth);
