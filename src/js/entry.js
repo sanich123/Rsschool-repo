1b35df0c5f034efc2991b6797e027d0094027a66
Alexander Voronin <aovoronin.piano@gmail.com>
2022-11-05 20:58:48 +0300

    feat: work on deploy

--- src/js/entry.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable linebreak-style */
-import '../audio/listing-page.mp3';
-import '../img/Фото резюме.jpg';
-import '@babel/polyfill';
-import './test';
-import '../index.html';
-import '../less/entry.less';
--- src/js/test.js
+++
@@ -1,8 +0,0 @@
-import sound from '../audio/listing-page.mp3';
-
-const p = document.querySelector('p');
-const audio = new Audio(sound);
-
-p.addEventListener('click', () => {
-  audio.play();
-});
