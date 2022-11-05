3944fbf24727a0438fc896dc91f498cba9d4a376
Alexander Voronin <aovoronin.piano@gmail.com>
2022-11-05 21:01:04 +0300

    feat: another work on deploy

---
+++ dist/asset/.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+import '../audio/listing-page.mp3';
+import '../img/Фото резюме.jpg';
+import '@babel/polyfill';
+import './test';
+import '../index.html';
+import '../less/entry.less';
