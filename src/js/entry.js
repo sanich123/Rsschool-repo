91148d49f0b4a561391f92f6975953b6f88e627e
Alexander Voronin <aovoronin.piano@gmail.com>
2022-11-06 19:40:29 +0300

    feat: work on startpage, mobile version

--- src/js/entry.js
+++ src/js/entry.js
@@ -5,7 +5,7 @@ import '../img/svg/beethoven-icon.svg';
 import '../img/svg/rs-school-icon.svg';
 
 // import '@babel/polyfill';
-import './test';
+
 import '../game.html';
 import '../index.html';
 import '../less/entry.less';
