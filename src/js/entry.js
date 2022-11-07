0f2c68274cdacdefacb043a279abaf361e945da1
sanich123 <aovoronin.piano@gmail.com>
2022-11-07 17:23:24 +0300

    feat: work adaptive layout for the game page

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,7 +1,11 @@
 /* eslint-disable linebreak-style */
 
+import '../audio/beethoven-5-symphony.mp3';
+import '../audio/tchaikovsky-adagio.mp3';
+
 import '../img/svg/beethoven-icon.svg';
 import '../img/svg/rs-school-icon.svg';
+import '../img/jpg/tchaikovsky.jpg';
 
 // import '@babel/polyfill';
 
