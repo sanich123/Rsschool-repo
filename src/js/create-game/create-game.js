381cb91ae055760442dc537aa6cd50f11306989b
sanich123 <aovoronin.piano@gmail.com>
2022-11-12 21:53:08 +0300

    feat: some work on right and wrong answers

--- src/js/create-game/create-game.js
+++ src/js/create-game/create-game.js
@@ -21,11 +21,24 @@ export function createGamePage(counter = 0, arr = birdsData, checkedAnswer = '',
     body.innerHTML = '';
 
     body.insertAdjacentHTML('afterbegin', `${createHeader()}${createMainGame(filtredBirds, checkedData, innerQuestionBird, innerChecked)}${createFooter()}`);
+    const answersLabels = document.querySelectorAll('.answers-list__input');
+    answersLabels.forEach((input) => {
+      if (checkedAnswer === innerQuestionBird.name && input.value === innerQuestionBird.name) {
+        input.parentNode.classList.add('accept');
+      }
+      if (input.value === checkedAnswer) {
+        input.parentNode.classList.add('decline');
+        input.parentNode.disabled = true;
+      }
+    });
 
     const navList = document.querySelector('.game__list');
     const answersList = document.querySelector('.answers-list');
   
     answersList.addEventListener('click', ({target}) => {
+      if (target.disabled) {
+        return;
+      }
       createGamePage(innerCounter, birdsData, target.value, innerQuestionBird);
     });
   
