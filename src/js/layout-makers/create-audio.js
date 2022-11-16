74f80070da813eb4e3e22ad474f0ba3c5aa4eafa
Alexander Voronin <aovoronin.piano@gmail.com>
2022-11-16 22:41:24 +0300

    feat: work on custom audio, almost done

--- src/js/layout-makers/create-audio.js
+++ src/js/layout-makers/create-audio.js
@@ -1,11 +1,16 @@
 export function createAudio(audio, place) {
-  return `<div class="audio-container">
-        <button class="audio-container__play play"></button>
-        <div class="audio-container__duration-container">
-        <input class="audio-container__duration-changer" type="range"/>
-        <span class="audio-container__duration">05:39</span>
-        <span class="audio-container__current-time">00:00</span>
-        </div>
-        <input class="audio-container__volume-changer" type="range" value="0" min="0" step="0"max="100"/>
-        <audio class="${place}__audio" src="${audio}"><audio/></div>`;
+  return `
+  <div class="audio-container">
+    <button class="audio-container__play play"></button>
+    <div class="audio-container__duration-container">
+      <input class="audio-container__duration-changer" type="range" min="0" value="0" max="100" />
+      <span class="audio-container__duration">05:39</span>
+      <span class="audio-container__current-time">00:00</span>
+    </div>
+    <div class="audio-container__mute-wrapper">
+    <button class="audio-container__mute-btn unmuted"></button>
+    <input class="audio-container__volume-changer" type="range" value="0.75" min="0" step="0.1" max="1"/>
+    </div>
+    <audio class="${place}__audio" src="${audio}"><audio/>
+  </div>`;
 }
--- src/js/manage-audio/manage-audio.js
+++ src/js/manage-audio/manage-audio.js
@@ -1,4 +1,3 @@
-import { createPauseIcon } from "../layout-makers/create-icons";
 import { getSeconds } from "../utils/helpers";
 
 export function setAudio() {
@@ -9,16 +8,49 @@ export function setAudio() {
   const totalDuration = document.querySelector(".question .audio-container__duration");
   const currentDuration = document.querySelector('.question .audio-container__current-time');
   const progress = document.querySelector('.audio-container__duration-changer');
+  const muteBtn = document.querySelector('.audio-container__mute-btn');
+  const volumeContainer = document.querySelector('.audio-container__mute-wrapper');
+  const audioProgress = document.querySelector('.audio-container__volume-changer');
+
+  muteBtn.addEventListener('click', () => {
+    if (muteBtn.classList.contains('unmuted')) {
+      muteBtn.classList.remove('unmuted');
+      muteBtn.classList.add('muted');
+      questionAudio.muted = true;
+    } else {
+      muteBtn.classList.remove('muted');
+      muteBtn.classList.add('unmuted');
+      questionAudio.muted = false;
+    }
+  });
+  volumeContainer.addEventListener('mouseenter', () => {
+    audioProgress.classList.add('is-open');
+  });
+  volumeContainer.addEventListener('mouseleave', () => {
+    audioProgress.classList.remove('is-open');
+  })
+  audioProgress.addEventListener('input', ({target}) => {
+    questionAudio.volume = target.value;
+    if (+target.value === 0) {
+      muteBtn.classList.remove('unmuted');
+      muteBtn.classList.add('muted');
+    } else if (muteBtn.classList.contains('muted')) {
+      muteBtn.classList.remove('muted');
+      muteBtn.classList.add('unmuted');
+    }
+  });
 
   questionAudio.addEventListener("loadeddata", () => {
     totalDuration.textContent = getSeconds(questionAudio.duration);
+    questionAudio.volume = '1';
     progress.value = 0;
     progress.max = Math.floor(+questionAudio.duration);
+    progress.step = questionAudio.duration / 100;
   });
-  progress.addEventListener('change', ({target}) => {
+  progress.addEventListener('input', ({target}) => {
     questionAudio.currentTime = target.value;
     currentDuration.textContent = getSeconds(target.value);
-  })
+  });
 
   questionPlay.addEventListener("click", () => {
     if (questionPlay.classList.contains("play")) {
@@ -27,7 +59,6 @@ export function setAudio() {
       questionAudio.play();
       setInterval(() => {
         progress.value = questionAudio.currentTime;
-        progress.step = questionAudio.duration / 100;
         currentDuration.textContent = getSeconds(questionAudio.currentTime);
       }, 1000);
     } else {
