72b1fa327060c416c298b320cf90170e3636946f
Alexander Voronin <aovoronin.piano@gmail.com>
2022-11-21 21:34:07 +0300

    feat: some work on deploy

--- dist/asset/.js
+++ dist/asset/.js
@@ -2,4 +2,5 @@ import '@babel/polyfill';
 import { createStartPage } from "./create-start-page/create-start-page.js";
 import "../index.html";
 import "../less/entry.less";
+console.log('Привет! Вроде кроме пункта: при выборе неправильного ответа проигрывание аудиоплеера не должно останавливаться реализовано.');
 createStartPage();
